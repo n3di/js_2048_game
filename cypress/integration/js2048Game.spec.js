61537916e39123876a283b965f44ac90180ffe01
mihau <github@nedi.me>
2025-04-19 02:45:05 +0200

    Solution correct

--- cypress/integration/js2048Game.spec.js
+++ cypress/integration/js2048Game.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Cypress.Commands.add('shuffleBoxes', (arrow1, arrow2, times) => {
-  cy.get('.button--restart').click();
+  cy.get('.button.start').click();
 
   for (let n = 0; n < times; n++) {
     cy.get('body').type(arrow1);
@@ -15,39 +15,39 @@ describe('2048 game UI', () => {
   });
 
   it('should show the initial score', () => {
-    cy.get('.button--start').click();
-    cy.get('.stats__score').should('have.value', '');
+    cy.get('.button.start').click();
+    cy.get('.game-score').should('have.value', '');
   });
 
   it('should show the score shuffling to the left and up only', () => {
     cy.shuffleBoxes('{leftArrow}', '{upArrow}', 3);
-    cy.get('.stats__score').invoke('text').then(parseFloat).should('be.gt', 1);
+    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
   });
 
   it('should show the score shuffling to the right and up only', () => {
     cy.shuffleBoxes('{rightArrow}', '{upArrow}', 3);
-    cy.get('.stats__score').invoke('text').then(parseFloat).should('be.gt', 1);
+    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
   });
 
   it('should show the score shuffling to the right and down only', () => {
     cy.shuffleBoxes('{rightArrow}', '{downArrow}', 3);
-    cy.get('.stats__score').invoke('text').then(parseFloat).should('be.gt', 1);
+    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
   });
 
   it('should show the score shuffling to the left and down only', () => {
     cy.shuffleBoxes('{leftArrow}', '{downArrow}', 3);
-    cy.get('.stats__score').invoke('text').then(parseFloat).should('be.gt', 1);
+    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
   });
 
   it('should reset the score', () => {
     cy.shuffleBoxes('{leftArrow}', '{downArrow}', 3);
-    cy.get('.stats__score').invoke('text').then(parseFloat).should('be.gt', 1);
-    cy.get('.button--restart').click();
-    cy.get('.stats__score').should('have.value', '');
+    cy.get('.game-score').invoke('text').then(parseFloat).should('be.gt', 1);
+    cy.get('.button.restart').click();
+    cy.get('.game-score').should('have.value', '');
   });
 
   it('should show message in case of the loss', () => {
-    cy.get('.button--restart').click();
+    cy.get('.button.start').click();
 
     for (let n = 0; n < 100; n++) {
       cy.get('body').type('{rightArrow}');
